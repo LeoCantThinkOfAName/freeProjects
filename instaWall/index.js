7685710fc76faae11f24cade294061f2c4f25e68
Monster Cookie <Monster Cookie>
2016-11-04 13:28:32 +0800

    deploy on heroku

--- instaWall/index.js
+++ instaWall/index.js
@@ -6,7 +6,7 @@ var methodOverride = require("method-override");
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({"extended": "true"}));
 app.use(bodyParser.json());
---
+++ instaWall/public/services/instagram.service.js
@@ -0,0 +1,18 @@
+angular.module("instaWall")
+    .factory("instagram", function($resource) {
+        return {
+            fetchPic: function(callback) {
+                var api = $resource("https://api.instagram.com/v1/users/self/media/recent/?&client_id=:client_id&callback=JSON_CALLBACK&access_token=:access_token", {
+                    client_id: "a83ee09ed6834c04ad7f4a07efb8136f",
+                    access_token: "698940771.1677ed0.b402c72193a146358884ba7d765b7640"
+                },
+                {
+                    fetch: {method: "JSONP"}
+                });
+
+                api.fetch(function(response) {
+                    callback(response.data);
+                })
+            }
+        }
+    })
